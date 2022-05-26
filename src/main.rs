75ef98e03cb7f374db448b928d4cfd1f491beaa1
Casey Koepp <cgk15@uakron.edu>
2022-05-26 18:35:31 -0400

    Modify webpage description

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,13 @@ fn app() -> Html {
     html! {
         <main>
             <h1>{ "Hello World!" }</h1>
-            <p>{ "This site is a work in progress." }</p>
+            <p>
+                { "This site is a work in progress." }
+                <br/>
+                { "It was made with the Rust language and using the Yew framework, go check out github repo " }
+                <a href="https://github.com/KC-plus-plus/KC-plus-plus.github.io">{ "here" }</a>
+                { " for more info." }
+            </p>
         </main>
     }
 }
